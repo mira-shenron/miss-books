9aa5e87c97083ff0731ad21964d26220c34f831b
Mira <mira.shenron@gmail.com>
2020-11-03 19:06:32 +0200

    miss-books

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -2,7 +2,7 @@ export default {
     template: `
     <section>
         <h1>Welcome!</h1>
-        <img src="img/books.jpg" width=500 height=500 alt="">
+        <img src="../../img/books.jpg" width=500 height=500 alt="">
     </section>
     `,
 }
